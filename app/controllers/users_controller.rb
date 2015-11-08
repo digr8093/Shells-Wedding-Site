245a713bfe6ff5419f3f9f991f29961a5f1758fe
Lauren <digr8093@github.com>
2015-11-08 17:15:24 -0600

    did light verison for create redirect2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
       if @user.save
 
         UserMailer.welcome_email(@user).deliver
-         format.html { redirect_to @user, notice: 'User was successfully created.' }
+         format.html { redirect_to @user, notice: 'You successfullu RSVPed.' }
          format.json { render :show, status: :created, location: @user }
 
       #  format.json { render :show, status: :created, location: @user }
